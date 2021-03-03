4f0c64beb2d69955a06fdc227e76ba6b8389d3dd
TimonPeng <timonminipeng@gmail.com>
2021-03-04 01:06:10 +0800

    fix your liquidity

--- src/components/YourLiquidity.vue
+++ src/components/YourLiquidity.vue
@@ -2,34 +2,30 @@
   <div class="card">
     <div class="card-body">
       <div v-if="!wallet.connected" class="fc-container">
-        <Button size="large" ghost @click="$store.dispatch('wallet/openModal')">
-          Unlock Wallet
-        </Button>
+        <Button size="large" ghost @click="$store.dispatch('wallet/openModal')"> Unlock Wallet </Button>
       </div>
       <Spin v-else :spinning="wallet.loading">
         <Icon slot="indicator" type="loading" style="font-size: 24px" spin />
 
-        <Collapse
-          v-for="liquidity in liquids"
-          :key="liquidity.mintAddress"
-          expand-icon-position="right"
-        >
-          <CollapsePanel class="liquidity-info" :header="liquidity.pool.name">
+        <Collapse v-for="info in liquids" :key="info.lp.mintAddress" expand-icon-position="right">
+          <CollapsePanel class="liquidity-info" :header="info.lp.symbol">
             <div class="fs-container">
               <div>Pooled:</div>
-              <div>1</div>
+              <div>≈ {{ info.userCoinBalance.format() }} {{ info.coin.symbol }}</div>
             </div>
             <div class="fs-container">
               <div>Pooled:</div>
-              <div>1</div>
+              <div>≈ {{ info.userPcBalance.format() }} {{ info.pc.symbol }}</div>
             </div>
             <div class="fs-container">
               <div>Your pool tokens:</div>
-              <div>{{ liquidity.user.balance.format() }}</div>
+              <div>{{ info.userLpBalance.format() }}</div>
             </div>
             <div class="fs-container">
               <div>Your pool share:</div>
-              <div>%</div>
+              <div>
+                ≈ {{ info.percent.isLessThan(0.0001) ? '&lt;0.01' : info.percent.multipliedBy(100).toFixed(2) }}%
+              </div>
             </div>
             <Row :gutter="32" class="actions">
               <Col :span="12">
@@ -42,9 +38,7 @@
           </CollapsePanel>
         </Collapse>
       </Spin>
-      <span>
-        If you staked your LP tokens in a farm, unstake them to see them here.
-      </span>
+      <span> If you staked your LP tokens in a farm, unstake them to see them here. </span>
     </div>
   </div>
 </template>
@@ -53,6 +47,9 @@
 import Vue from 'vue'
 import { mapState } from 'vuex'
 import { Button, Collapse, Row, Col, Spin, Icon } from 'ant-design-vue'
+import { cloneDeep, get } from 'lodash-es'
+
+import { TokenAmount } from '@/utils/safe-math'
 
 const CollapsePanel = Collapse.Panel
 
@@ -64,18 +61,18 @@ export default Vue.extend({
     Row,
     Col,
     Spin,
-    Icon,
+    Icon
   },
 
   data() {
     return {
       loading: false,
-      liquids: [] as any,
+      liquids: [] as any
     }
   },
 
   computed: {
-    ...mapState(['wallet']),
+    ...mapState(['wallet', 'liquidity'])
   },
 
   watch: {
@@ -83,8 +80,8 @@ export default Vue.extend({
       handler(newTokenAccounts: any) {
         this.updateLiquids(newTokenAccounts)
       },
-      deep: true,
-    },
+      deep: true
+    }
   },
 
   mounted() {
@@ -92,29 +89,47 @@ export default Vue.extend({
   },
 
   methods: {
-    updateLiquids(_tokenAccounts: any) {
-      const liquids = [] as any
+    updateLiquids(tokenAccounts: any) {
+      let liquids = [] as any
 
-      // for (const [mintAddress, tokenAccount] of Object.entries(tokenAccounts)) {
-      //   const liquidityPool = Liquidity.getByLpMintAddress(mintAddress)
+      for (const [mintAddress, tokenAccount] of Object.entries(tokenAccounts)) {
+        const poolInfo = get(this.liquidity.infos, mintAddress)
 
-      //   if (liquidityPool) {
-      //     // @ts-ignore
-      //     liquids.push({
-      //       ...{ pool: liquidityPool },
-      //       ...{ user: tokenAccount },
-      //     })
-      //   }
-      // }
+        if (poolInfo) {
+          const lp = cloneDeep(poolInfo.lp)
+          const coin = cloneDeep(poolInfo.coin)
+          const pc = cloneDeep(poolInfo.pc)
 
-      // liquids = liquids.filter(
-      //   // @ts-ignore
-      //   (liquidity) => !liquidity.user.balance.wei.isZero()
-      // )
+          const userLpBalance = cloneDeep(tokenAccount.balance)
+          const lpCoinBalance = cloneDeep(coin.balance)
+          const lpPcBalance = cloneDeep(pc.balance)
+
+          const percent = userLpBalance.wei.dividedBy(lp.totalSupply.wei)
+          const userCoinBalance = new TokenAmount(lpCoinBalance.wei.multipliedBy(percent), lpCoinBalance.decimals)
+          const userPcBalance = new TokenAmount(lpPcBalance.wei.multipliedBy(percent), lpPcBalance.decimals)
+
+          liquids.push({
+            lp,
+            coin,
+            pc,
+
+            userCoinBalance,
+            userPcBalance,
+            userLpBalance,
+            percent
+          })
+        }
+      }
+
+      liquids = liquids
+        .filter((liquidity: any) => !liquidity.userLpBalance.wei.isZero())
+        .sort((a: any, b: any) => {
+          return b.userLpBalance.toEther() - a.userLpBalance.toEther()
+        })
 
       this.liquids = liquids
-    },
-  },
+    }
+  }
 })
 </script>
 
