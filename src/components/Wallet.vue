f1ab92722d06cb6cb706bcc56be0cea27c7bb252
Tadeas Kmenta <tadeas.kmenta@zel.cash>
2021-06-02 18:35:32 +0800

    integrate zelcore

--- src/components/Wallet.vue
+++ src/components/Wallet.vue
@@ -76,7 +76,8 @@ export default class Wallet extends Vue {
     Phantom: '',
     Sollet: 'https://www.sollet.io',
     // Solflare: 'https://solflare.com/access-wallet',
-    Bonfida: 'https://bonfida.com/wallet'
+    Bonfida: 'https://bonfida.com/wallet',
+    Zelcore: 'https://link.zelcore.io'
     // https://docs.coin98.app/coin98-extension/developer-guide
     // Coin98: ''
     // ezDeFi: '',
