fd1c17926bb53440292f0ad53feebfd004e551b0
zhanggongying <edsolater@qq.com>
2021-07-29 18:29:43 +0800

    chore(page guide): change for new UI style

--- src/pages/guide.vue
+++ src/pages/guide.vue
@@ -1,22 +1,85 @@
 <template>
   <div class="guide">
-    <section class="title-part">
-      <div class="sub-leading">Learn, Trade, Follow, Refer and Earn RAY</div>
-      <div class="main-title">Raydium Bounty Airdrop</div>
-      <div class="additional-description">in partnership with <img src="../assets/icons/binance-test-logo.svg" /></div>
+    <section class="page-title">
+      <div class="page-sub-leading">Learn, Trade, Follow, Refer and Earn RAY</div>
+      <div class="page-main-title">Raydium Bounty Airdrop</div>
+      <div class="page-additional-description">
+        in partnership with <img src="../assets/icons/binance-test-logo.svg" />
+      </div>
+    </section>
+
+    <section class="TLDR">
+      <div class="title">Earn up to $2,000 in RAY in 4 easy steps</div>
+      <table class="TLDR-table">
+        <tr>
+          <th class="th"></th>
+          <th class="th">Task</th>
+          <th class="th">Points</th>
+        </tr>
+        <tr>
+          <td class="td order">1</td>
+          <td class="td">Follow Socials</td>
+          <td class="td"><div class="point-label">+2 POINTS</div></td>
+        </tr>
+        <tr>
+          <td class="td order">2</td>
+          <td class="td">Swap on Raydium</td>
+          <td class="td"><div class="point-label">+7 POINTS</div></td>
+        </tr>
+        <tr>
+          <td class="td order">3</td>
+          <td class="td">Refer Friends</td>
+          <td class="td"><div class="point-label">+1 POINTS / REFERRAL</div></td>
+        </tr>
+        <tr>
+          <td class="td order">4</td>
+          <td class="td">Trade 250 RAY</td>
+          <td class="td"><div class="point-label">+3 POINTS</div></td>
+        </tr>
+      </table>
+      <div class="footnote">
+        *** Binance UID is required to receive airdrop. Don’t forget to enter your Binance account below.
+      </div>
+    </section>
+
+    <section class="media-entries">
+      <div class="task-level">Task 1</div>
+      <div class="title">Join Forces With Raydium</div>
+      <div class="subtitle">Total Points +2</div>
+      <div class="box-grid">
+        <div class="box social-media">
+          <div class="box-title">
+            Follow & Retweet Raydium
+            <div class="point-label">+1 POINTS</div>
+          </div>
+          <button><img class="icon" src="../assets/icons/guide-twitter-icon.svg" />GO TO TWITTER</button>
+        </div>
+        <div class="box social-media">
+          <div class="box-title">
+            Join the Raydium Discord
+            <div class="point-label">+1 POINTS</div>
+          </div>
+          <button><img class="icon" src="../assets/icons/guide-discord-icon.svg" />JOIN DISCORD</button>
+        </div>
+      </div>
     </section>
 
     <section class="step-game">
-      <div class="btns">
-        <button class="primary">GET STARTED</button>
-        <button>REWARD DETAILS</button>
-      </div>
-      <svg class="p-1" viewBox="0 0 532 132">
+      <div class="task-level">Task 2</div>
+      <div class="title">Try Raydium’s Light Speed Swaps</div>
+      <div class="subtitle">Total Points +7</div>
+      <!-- <div class="btns">
+        <button class="primary" @click="to('step-1')">GET STARTED</button>
+        <button @click="to('details')">REWARD DETAILS</button>
+      </div> -->
+      <!-- <svg class="p-1" viewBox="0 0 532 132">
         <polyline points="126,0 126,66 266,65 266,132" fill="none" stroke-width="2" stroke-dasharray="12" />
-      </svg>
-      <div class="step-box watch-video">
-        <div class="step-label">Step 1</div>
-        <div class="step-title">Watch how to create a wallet on Solana and make your first Swap on Raydium.</div>
+      </svg> -->
+      <div ref="step-1" class="box watch-video">
+        <!-- <div class="step-label">Step 1</div> -->
+        <div class="box-title has-step">
+          Watch how to create a wallet on Solana and make your first Swap on Raydium.
+        </div>
         <div class="video-box">
           <div
             v-if="!isVideoPlaying"
@@ -44,77 +107,89 @@
           />
         </div>
       </div>
-      <svg class="p-2" viewBox="0 0 532 132">
+
+      <svg class="step-gap-line" viewBox="0 0 532 132">
         <polyline points="266,0 266,132" fill="none" stroke-width="2" stroke-dasharray="12" />
       </svg>
-      <div class="step-box -2">
-        <div class="step-label">Step 2</div>
-        <div class="step-title">Swap on Raydium</div>
-        <button>GO TO SWAP</button>
+
+      <div class="box wallet">
+        <!-- <div class="step-label">Step 2</div> -->
+        <div class="box-title has-step">Connect your Wallet</div>
+        <Wallet />
       </div>
-      <svg class="p-3" viewBox="0 0 532 132">
+
+      <svg class="step-gap-line" viewBox="0 0 532 132">
         <polyline points="266,0 266,132" fill="none" stroke-width="2" stroke-dasharray="12" />
       </svg>
-      <div class="step-box form">
-        <div class="step-label">Step 3</div>
-        <div class="step-title">Fill out the form to enter</div>
+
+      <div class="box">
+        <!-- <div class="step-label">Step 2</div> -->
+        <div class="box-title has-step">
+          Swap on Raydium
+          <div class="point-label">+7 POINTS</div>
+        </div>
+        <NuxtLink to="/swap/">
+          <button>GO TO SWAP</button>
+        </NuxtLink>
+      </div>
+    </section>
+
+    <section class="referrals">
+      <div class="task-level">Task 3</div>
+      <div class="title">Maximize Points By Referring Friends</div>
+      <div class="subtitle">Referral Board Top 10 Earn $2,000 in RAY, each!</div>
+      <div class="box">
+        <div class="box-title">
+          Refer now with your customized link below
+          <div class="point-label">+1 POINTS / REFERRAL</div>
+        </div>
+        <div class="icon-btns">
+          <button class="icon-btn">
+            <a
+              href="https://docs.google.com/forms/d/1Mk-x0OcI1tCZzL0Lj_WY8d02dMXsc-Z2AG3AaO6W_Rc/edit#responses"
+              rel="nofollow noopener noreferrer"
+              target="_blank"
+              ><img class="icon" src="../assets/icons/guide-facebook-icon.svg"
+            /></a>
+          </button>
+          <button class="icon-btn"><img class="icon" src="../assets/icons/guide-twitter-icon-2.svg" /></button>
+          <button class="icon-btn"><img class="icon" src="../assets/icons/guide-share-icon.svg" /></button>
+        </div>
+      </div>
+    </section>
+
+    <section class="binance-id-form">
+      <div class="task-level">Task 4</div>
+      <div class="title">Trade on Binance</div>
+      <div class="box form">
+        <div class="box-title">Binance UID is Required to Receive Airdrop</div>
         <div class="input-box">
           <label>Binance User ID</label><a rel="nofollow noopener noreferrer">Where do I find it?</a>
           <input v-model="inputContent" />
         </div>
         <div class="check-box">
           <Checkbox v-model="inputChecked" class="checkbox" />
-          <div class="text">
-            I am the owner of this Binance UID and have decided that I want to share my Solana Wallet Address to receive
-            +150 points for swapping on Raydium.
-          </div>
+          <div class="text">I am the owner of this Binance account</div>
         </div>
         <button>SUBMIT</button>
       </div>
-    </section>
 
-    <section class="media-entries">
-      <div class="title">EARN MORE POINT BY COMPLETING THE FOLLOWING TASKS</div>
-      <div class="box-grid">
-        <div class="box">
-          <div class="box-title">Compete in the RAY Trading Competition on Binance</div>
-          <div class="point-label">+100 POINTS</div>
-          <button :style="{ '--secondary': '#f3ba2f' }">
-            <img class="icon" src="../assets/icons/guide-binance-icon.svg" />GO TO BINANCE
-          </button>
-        </div>
-        <div class="box">
-          <div class="box-title">Follow Raydium on Twitter</div>
-          <div class="point-label">+10 POINTS</div>
-          <button><img class="icon" src="../assets/icons/guide-twitter-icon.svg" />GO TO TWITTER</button>
-        </div>
-        <div class="box">
-          <div class="box-title">Join the Raydium Discord</div>
-          <div class="point-label">+10 POINTS</div>
-          <button><img class="icon" src="../assets/icons/guide-discord-icon.svg" />JOIN DISCORD</button>
-        </div>
-        <div class="box">
-          <div class="box-title">Retweet Raydium</div>
-          <div class="point-label">+5 POINTS</div>
-          <button><img class="icon" src="../assets/icons/guide-retweet-icon.svg" />RETWEET</button>
-        </div>
-      </div>
-    </section>
+      <svg class="step-gap-line" viewBox="0 0 532 132">
+        <polyline points="266,0 266,132" fill="none" stroke-width="2" stroke-dasharray="12" />
+      </svg>
 
-    <section class="referrals">
-      <div class="title">EARN BONUS POINTS FOR REFERRALS</div>
       <div class="box">
-        <div class="box-title">Share this site with friends via your customized links below</div>
-        <div class="point-label">+25 POINTS/ REFERRAL</div>
-        <div class="icon-btns">
-          <div class="icon-btn"><img class="icon" src="../assets/icons/guide-facebook-icon.svg" /></div>
-          <div class="icon-btn"><img class="icon" src="../assets/icons/guide-twitter-icon-2.svg" /></div>
-          <div class="icon-btn"><img class="icon" src="../assets/icons/guide-share-icon.svg" /></div>
+        <div class="box-title">
+          Trade 250 RAY on Binance
+          <div class="point-label">+3 POINTS</div>
         </div>
+        <button :style="{ '--secondary': '#f3ba2f' }">
+          <img class="icon" src="../assets/icons/guide-binance-icon.svg" />GO TO BINANCE
+        </button>
       </div>
     </section>
 
-    <section class="reward-details">
+    <section ref="details" class="reward-details">
       <div class="title">REWARD DETAILS</div>
       <ol>
         <li>The first 4,000 users to be verified as completing all 4 tasks will win $20 in RAY tokens each.</li>
@@ -250,6 +325,12 @@ export default class Guide extends Vue {
   isVideoPlaying = false
   inputContent = ''
   inputChecked = false
+
+  to(anchor: 'step-1' | 'details') {
+    const el = this.$refs[anchor] as Element | undefined
+    if (!el) return
+    el.scrollIntoView({ behavior: 'smooth' })
+  }
 }
 </script>
 
@@ -266,7 +347,7 @@ export default class Guide extends Vue {
   --secondary: #39d0d8;
   --primary-text: #fff;
   --secondary-text: #c4d6ff;
-  --video-bg: #231c73;
+  --video-bg: hsla(0, 0%, 0%, 0.8);
   --point-label-bg: #da2eef;
   --reward-card-bg: #191542;
 
@@ -274,10 +355,15 @@ export default class Guide extends Vue {
   margin-right: 80px;
   min-height: 100vh;
 }
+
 section {
+  padding-top: 32px;
+  padding-bottom: 120px;
   display: grid;
   justify-items: center;
+  position: relative;
 }
+
 button {
   padding: 8px 40px;
   border: 2px solid var(--secondary);
@@ -305,6 +391,48 @@ button {
   }
 }
 
+.task-level {
+  font-weight: 500;
+  font-size: 36px;
+  color: var(--secondary-text);
+  opacity: 0.7;
+  position: absolute;
+  top: 0;
+  left: 0;
+  &::after {
+    position: absolute;
+    content: '';
+    bottom: 0;
+    right: -16px;
+    width: 60%;
+    height: 2px;
+    background-color: var(--secondary);
+  }
+}
+
+.title {
+  font-weight: 500;
+  font-size: 28px;
+  line-height: 31px;
+  color: var(--primary-text);
+  margin-top: 40px;
+  margin-bottom: 56px;
+  position: relative;
+  max-width: 800px;
+  text-align: center;
+}
+
+.subtitle {
+  font-weight: 500;
+  font-size: 18px;
+  color: var(--secondary-text);
+  margin-top: -40px;
+  margin-bottom: 56px;
+  position: relative;
+  max-width: 400px;
+  text-align: center;
+}
+
 .point-label {
   padding: 4px 12px;
   display: inline-block;
@@ -314,216 +442,178 @@ button {
   font-weight: 500;
   font-size: 14px;
   line-height: 18px;
+
+  .box-title & {
+    margin-left: 32px;
+  }
 }
 
-.title-part {
+.page-title {
   margin-top: 64px;
-  margin-bottom: 52px;
-
-  .sub-leading {
+  padding-bottom: 48px;
+  .page-sub-leading {
     font-size: 20px;
     line-height: 26px;
     color: var(--secondary);
     margin-bottom: 12px;
   }
-  .main-title {
+  .page-main-title {
     font-weight: 500;
     font-size: 56px;
     line-height: 60px;
     color: var(--primary-text);
     margin-bottom: 8px;
   }
-  .additional-description {
+  .page-additional-description {
     font-size: 24px;
     line-height: 31px;
   }
 }
 
-.step-game {
-  margin-bottom: 156px;
-
-  .btns {
-    display: grid;
-    grid-auto-flow: column;
-    gap: 40px;
-  }
-  button {
-    width: 100%;
-  }
-
-  .step-box {
-    padding: 24px;
-    border: 2px solid var(--secondary);
-    box-shadow: 12px 12px 0 var(--secondary);
-    border-radius: 20px;
-    background: var(--primary);
-    position: relative;
-
-    .step-label {
-      position: absolute;
-      right: -0.5px; // if 0 , there may be a slim gap
-      top: 24px;
-      padding: 8px 12px;
-      border-radius: 8px 0 0 8px;
-      background: var(--secondary);
-      font-weight: 500;
-      font-size: 24px;
-      line-height: 28px;
-      color: var(--primary);
-    }
-    .step-title {
-      font-weight: 500;
-      font-size: 24px;
-      line-height: 28px;
-      color: var(--primary-text);
-      margin-right: 144px;
-      max-width: 316px;
-      margin-bottom: 40px;
-    }
-    &.watch-video {
-      .video-box {
-        position: relative;
-        .play-icon {
-          cursor: pointer;
-          position: absolute;
-          z-index: 1;
-          inset: 0;
-          margin: auto;
-          width: 80px;
-          height: 80px;
-        }
-        video {
-          border-radius: 20px;
-          background-color: var(--video-bg);
-          width: 100%;
-        }
-      }
-    }
-    &.form {
-      max-width: 580px;
-      .input-box {
-        display: grid;
-        grid-template-columns: 1fr 1fr;
-        margin-bottom: 24px;
-        label {
-          justify-self: start;
-        }
-        a {
-          justify-self: end;
-          text-decoration: underline;
-        }
-        input {
-          margin-top: 8px;
-          border: 1.5px solid var(--secondary-text);
-          caret-color: var(--secondary);
-          padding: 8px;
-          border-radius: 8px;
-          grid-column: span 2;
-          background-color: transparent;
-          color: var(--secondary-text);
-        }
-      }
-      .check-box {
-        display: flex;
-        align-items: center;
-        margin-bottom: 32px;
-        .checkbox {
-          margin-right: 16px;
-        }
-        .text {
-          font-size: 12px;
-          line-height: 15px;
-          color: var(--secondary-text);
-        }
-      }
-    }
-  }
-  .p-1,
-  .p-2,
-  .p-3 {
-    width: 532px;
-    height: 132px;
-    stroke: var(--secondary);
-  }
-}
 .box {
-  max-width: 552px;
-  padding: 32px 32px 40px;
-  border: 2px solid var(--secondary);
-  border-radius: 20px;
-  background: var(--primary);
   position: relative;
-  display: grid;
-  grid-template:
-    'title  point ' auto
-    'button button' auto / 1fr auto;
-  gap: 32px;
-  align-items: center;
+  padding: 24px;
+  border: 2px solid var(--secondary);
+  box-shadow: 12px 12px 0 var(--secondary);
+  background: var(--primary);
+  max-width: 552px;
+  border-radius: 20px;
 
   .box-title {
     font-weight: 500;
     font-size: 24px;
-    line-height: 28px;
+    line-height: 36px;
     color: var(--primary-text);
+    margin-bottom: 36px;
+    &.has-step {
+      margin-right: 144px;
+    }
   }
-  button {
+
+  button:not(.icon-btn) {
+    width: 100%;
     grid-area: button;
     align-self: flex-end;
+  }
+
+  .step-label {
+    position: absolute;
+    right: -0.5px; // if 0 , there may be a slim gap
+    top: 24px;
+    padding: 8px 12px;
+    border-radius: 8px 0 0 8px;
+    background: var(--secondary);
+    font-weight: 500;
+    font-size: 24px;
+    line-height: 28px;
+    color: var(--primary);
+  }
+
+  &.watch-video {
+    max-width: 800px;
+    .video-box {
+      position: relative;
+      .play-icon {
+        cursor: pointer;
+        position: absolute;
+        z-index: 1;
+        inset: 0;
+        margin: auto;
+        width: 80px;
+        height: 80px;
+      }
+      video {
+        border-radius: 20px;
+        background-color: var(--video-bg);
+        width: 100%;
+      }
+    }
+  }
+
+  &.form {
+    max-width: 580px;
+    .input-box {
+      display: grid;
+      grid-template-columns: 1fr 1fr;
+      margin-bottom: 24px;
+      label {
+        justify-self: start;
+      }
+      a {
+        justify-self: end;
+        text-decoration: underline;
+      }
+      input {
+        margin-top: 8px;
+        border: 1.5px solid var(--secondary-text);
+        caret-color: var(--secondary);
+        padding: 8px;
+        border-radius: 8px;
+        grid-column: span 2;
+        background-color: transparent;
+        color: var(--secondary-text);
+      }
+    }
+    .check-box {
+      display: flex;
+      align-items: center;
+      margin-bottom: 32px;
+      .checkbox {
+        margin-right: 16px;
+      }
+      .text {
+        font-size: 12px;
+        line-height: 15px;
+        color: var(--secondary-text);
+      }
+    }
+  }
+}
+
+.step-gap-line {
+  width: 532px;
+  height: 132px;
+  stroke: var(--secondary);
+}
+
+.TLDR {
+  margin: 64px auto 120px;
+  max-width: 880px;
+  border-radius: 20px;
+  border: 2px solid var(--secondary);
+  padding: 24px 48px;
+
+  table.TLDR-table {
+    width: 60%;
+    margin-bottom: 48px;
+    th.th {
+      padding: 12px 16px;
+      color: var(--secondary-text);
+    }
+    td.td {
+      padding: 4px 16px;
+    }
+  }
+
+  .footnote {
+    color: var(--secondary-text);
+    font-size: 14px;
+    line-height: 18px;
   }
 }
 
 .media-entries {
-  margin-bottom: 144px;
-  .title {
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 31px;
-    color: var(--primary-text);
-    margin-bottom: 72px;
-    position: relative;
-    max-width: 400px;
-    text-align: center;
-    &::after {
-      position: absolute;
-      content: '';
-      bottom: -8px;
-      right: 48px;
-      width: 60%;
-      height: 2px;
-      background-color: var(--secondary);
-    }
-  }
   .box-grid {
     display: grid;
     grid-template-columns: 1fr 1fr;
-    gap: 24px;
+    gap: 96px;
   }
 }
 
 .referrals {
   margin-bottom: 120px;
-  .title {
-    font-weight: 500;
-    font-size: 24px;
-    line-height: 31px;
-    color: var(--primary-text);
-    margin-bottom: 72px;
-    position: relative;
-    max-width: 420px;
-    text-align: center;
-    &::after {
-      position: absolute;
-      content: '';
-      bottom: -8px;
-      right: -32px;
-      width: 60%;
-      height: 2px;
-      background-color: var(--secondary);
-    }
-  }
-  .box .point-label {
-    max-width: 116px;
-    padding: 4px 16px;
+  .box {
+    max-width: 432px;
   }
   .icon-btns {
     grid-area: button;
@@ -624,6 +714,27 @@ button {
       color: var(--secondary-text);
       margin-bottom: 8px;
     }
+  }
+}
+</style>
+
+<style lang="less">
+.guide .wallet button {
+  width: 100%;
+  border: 2px solid var(--secondary);
+  border-radius: 8px;
+  padding: 8px 16px;
+  height: auto;
+  font-weight: 500;
+  font-size: 20px;
+  line-height: 26px;
+  color: var(--secondary);
+  &:hover {
+    filter: brightness(0.8);
+  }
+  &:active {
+    filter: brightness(0.5);
+    transform: translate(1px, 1px);
   }
 }
 </style>
