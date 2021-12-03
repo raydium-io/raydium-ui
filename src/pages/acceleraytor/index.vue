09489dca5bf7fec2d41c520f7c97f1aab80ebd0b
Rudy <lengxin5348@gmail.com>
2021-12-03 16:16:49 +0800

    update accele raytor page

--- src/pages/acceleraytor/index.vue
+++ src/pages/acceleraytor/index.vue
@@ -13,25 +13,6 @@
         <Carousel autoplay dots-class="carousel-dots">
           <div>
             <div class="inner-decorator-words">
-              Pools open Dec 1, 12:00 UTC - Full details
-              <a
-                href="https://raydium.medium.com/tabtrader-is-launching-on-acceleraytor-bc570b6a9628"
-                rel="nofollow noopener noreferrer"
-                target="_blank"
-              >
-                here</a
-              >
-            </div>
-            <a
-              href="https://raydium.medium.com/tabtrader-is-launching-on-acceleraytor-bc570b6a9628"
-              rel="nofollow noopener noreferrer"
-              target="_blank"
-            >
-              <img class="carousel-image" src="../../assets/background/acceleraytor-placeholder2.webp" />
-            </a>
-          </div>
-          <div>
-            <div class="inner-decorator-words">
               Pools open Dec 6, 12:00 UTC - Full details
               <a
                 href="https://raydium.medium.com/runnode-is-launching-on-acceleraytor-3ff7326864b0"
