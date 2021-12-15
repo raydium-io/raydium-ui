e494c6286b732592071eb2c997d91078bb8e508b
Rudy <lengxin5348@gmail.com>
2021-12-15 20:52:05 +0800

    update pools page

--- src/pages/pools.vue
+++ src/pages/pools.vue
@@ -185,7 +185,7 @@ export default class Pools extends Vue {
     for (const item of this.pools) {
       if (
         !item.name.includes('unknown') &&
-        item.liquidity !== 0 &&
+        item.liquidity > 0 &&
         (this.searchName === '' || item.name.toLowerCase().includes(this.searchName.toLowerCase().trim()))
       ) {
         pool.push(item)
