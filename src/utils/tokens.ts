cc971a450568dede2f94e7cdf8b7cfea923d6a95
rudy <lengxin5348@gmail.com>
2022-01-05 08:15:33 +0800

    add SHDW token

--- src/utils/tokens.ts
+++ src/utils/tokens.ts
@@ -1172,6 +1172,13 @@ export const TOKENS: Tokens = {
     mintAddress: 'HKfs24UEDQpHS5hUyKYkHd9q7GY5UQ679q2bokeL2whu',
     decimals: 6,
     tags: ['raydium']
+  },
+  SHDW: {
+    symbol: 'SHDW',
+    name: 'SHDW',
+    mintAddress: 'SHDWyBxihqiCj6YekG2GUr7wqKLeLAMK1gHZck9pL6y',
+    decimals: 9,
+    tags: ['raydium']
   }
 }
 
