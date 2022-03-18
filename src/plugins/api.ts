48162831aa9ac906bc0ff6d6bdd56d1aeebcfa4d
rudy <lengxin5348@gmail.com>
2022-03-18 20:47:43 +0800

    update info api

--- src/plugins/api.ts
+++ src/plugins/api.ts
@@ -6,9 +6,9 @@ const VERSION = '1.1.0'
 const apiPlugin: Plugin = (ctx, inject) => {
   const api: NuxtApiInstance = {
     getPrices: () => ctx.$axios.get('https://api.raydium.io/v1/main/price'),
-    getInfo: () => ctx.$axios.get('https://api.raydium.io/info'),
+    getInfo: () => ctx.$axios.get('https://api.raydium.io/v1/main/info'),
     getPairs: () => ctx.$axios.get('https://api.raydium.io/pairs'),
-    getConfig: () => ctx.$axios.get('https://api.raydium.io/config', { params: { v: VERSION } }),
+    getConfig: () => ctx.$axios.get('https://api.raydium.io/v1/main/config', { params: { v: VERSION } }),
     getEpochInfo: (rpc: string) => ctx.$axios.post(rpc, { jsonrpc: '2.0', id: 1, method: 'getEpochInfo' }),
     getCompaign: ({ campaignId = 2, address, referral }) =>
       ctx.$axios.get(`https://api.raydium.io/campaign/${campaignId}`, { params: { address, referral } }),
