9b549b833d6282de627239fcb7ffa2b4458d74f0
rudy <lengxin5348@gmail.com>
2022-04-21 16:40:45 +0800

    update rpc

--- src/utils/web3.ts
+++ src/utils/web3.ts
@@ -21,8 +21,7 @@ import BigNumber from 'bignumber.js'
 export const web3Config = {
   strategy: 'speed',
   rpcs: [
-    { url: 'https://raydium.rpcpool.com', weight: 50 },
-    { url: 'https://raydium.genesysgo.net', weight: 30 },
+    { url: 'https://raydium.rpcpool.com', weight: 80 },
     { url: 'https://solana-api.projectserum.com', weight: 10 },
     { url: 'https://solana-api.tt-prod.net', weight: 10 }
   ]
