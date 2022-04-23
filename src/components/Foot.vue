53406fbac18333c4fa0d30364943a1e5be9a3f5a
rudy <lengxin5348@gmail.com>
2022-04-23 12:33:05 +0800

    update discord link

--- src/components/Foot.vue
+++ src/components/Foot.vue
@@ -52,7 +52,7 @@ export default class Foot extends Vue {
     info: '/info/',
     listing: 'https://forms.gle/Ct7ySJkxQyUvrHR99',
     twitter: 'https://twitter.com/RaydiumProtocol',
-    discord: 'https://discord.gg/mEPnd8chex',
+    discord: 'https://discord.com/invite/raydiumprotocol',
     telegram: 'https://t.me/raydiumprotocol',
     medium: 'https://raydium.medium.com/'
   }
